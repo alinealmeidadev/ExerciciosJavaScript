e9765bdf6d35541e54fe53fa6705aeb246efe2c4
alinealmeida <alinealmeidadev@gmail.com>
2022-05-31 23:02:43 -0300

    commit do exercício

---
+++ evercicio.js
@@ -0,0 +1,7 @@
+const materia = "Lógiga de Programação";
+const notaMaxima = 6;
+const notaProva = 5.3;
+const minAprovacao = 4.001;
+const aprovacao = true;
+
+console.log("Na matéria de " + materia + " a nota máxima era " + notaMaxima + " eu tirei " + notaProva + " e o mínimo para aprovação era " + minAprovacao + ". Sendo assim, a afirmação de que passei é " + aprovacao);
